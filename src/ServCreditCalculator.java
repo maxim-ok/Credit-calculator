de51615b25574115a35a45872797b2bc770a3398
Maksim Okunev <max.vic.okunev@gmail.com>
2022-01-16 20:07:44 +0300

    Third commit

--- src/ServCreditCalculator.java
+++ src/ServCreditCalculator.java
@@ -1,13 +1,12 @@
 public class ServCreditCalculator {
-    public double calculate ( int amountOfCredit, int numOfMonths, double rate ) {
+    public double calculate(int amountOfCredit, int numOfMonths, double rate) {
 
-        double monthRate = rate/100/12; //Расчитываем месячную процентную ставку
-        double exponentiation = Math.pow( (1+monthRate), numOfMonths); //Расчитывам "степнную" составляющую из формулы расчета аннуитеного платежа
+        double monthRate = rate / 100 / 12; //Расчитываем месячную процентную ставку
+        double exponentiation = Math.pow((1 + monthRate), numOfMonths); //Расчитывам "степнную" составляющую из формулы расчета аннуитеного платежа
 
-        double annuity = monthRate + ( monthRate/(exponentiation-1)); //Расчитываем аннуитетный коэффициент
-        //double everyMonthMoney =  amountOfCredit * annuity; //Расчитываем ежемесячный платеж
+        double annuity = monthRate + (monthRate / (exponentiation - 1)); //Расчитываем аннуитетный коэффициент
 
-        return  Math.round(amountOfCredit * annuity);
+        return Math.round(amountOfCredit * annuity);
 
     }
 
